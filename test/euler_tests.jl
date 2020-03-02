a83e8536650b3bc3dcea6e527f05a28d106f11b1
DarioDN <dario.di.nardo.1993@gmail.com>
2020-03-02 10:31:14 +0100

    relazione finale + refactoring

---
+++ test/euler_tests.jl
@@ -0,0 +1,4 @@
+include("./euler_ten_planar_polygons.jl")
+include("./euler_two_concentric_cubes.jl")
+include("./euler_two_cubes_common_vertex.jl")
+include("./euler_two_random_cubes.jl")
---
+++ test/solid_tests.jl
@@ -0,0 +1,4 @@
+include("./octahedron_test.jl")
+include("./tetramihexahedron_test.jl")
+include("./octahemioctahedron_test.jl")
+include("./cubohemioctahedron_prototype.jl")
